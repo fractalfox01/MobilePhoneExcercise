07499162e53189d190a8b42ee849fcbdffab354d
fractalfox01 <flgtom@aol.com>
2017-08-12 07:03:34 -0700

    Problems in removeContact: can't use foreach on ArrayList<>

--- src/Contacts.java
+++ src/Contacts.java
@@ -6,6 +6,7 @@ public class Contacts {
     private String name;
     private String phone;
     private Scanner scan = new Scanner(System.in);
+    private String[] currentContact = new String[2];
 
     public Contacts() {
 
@@ -36,7 +37,7 @@ public class Contacts {
                     updateExisting();
                     break;
                 case 5:
-                    System.out.println("\n\n************************\n\tExiting Mobile Phone\n\n************************\n\t");
+                    System.out.println("\n\n****************************\n\tExiting Mobile Phone\n****************************\n\t");
                     System.exit(0);
                     break;
                 default:
@@ -49,11 +50,61 @@ public class Contacts {
 
     private void removeContact() {
         System.out.println("\n\n----------Removing Contact----------\n");
+        System.out.print("Enter Name: ");
+        String tempName = scan.next();
+        scan.nextLine();
+        System.out.print("Enter Phone: ");
+        String tempPhone = scan.next();
+
+        int tmpInt;
+        //Can't do the following foreach loop below. contacts needs to be iterated though I believe.
+        // In its current state the following throws:
+        //      Exception in thread "main" java.util.ConcurrentModificationException
+        //      at java.util.ArrayList$Itr.checkForComodification(ArrayList.java:901)
+        //      at java.util.ArrayList$Itr.next(ArrayList.java:851)
+        for (String w:contacts){
+            if (w.contains(tempName) && w.contains(tempPhone)){
+                String tmp = ("Name: " + tempName + "\t\t\tPhone: " + tempPhone);
+                try {
+                    contacts.remove(tmp);
+
+                    System.out.println("\nContact Removed\n");
+                    viewAllContact();
+                }
+                catch (Exception e){
+                    System.out.println(e);
+                }
+            }
+        }
     }
 
     private void addNewContact() {
-        System.out.println("\n\n----------Adding New Contact----------\n");
+        System.out.print("Enter New Contact Name: ");
+        this.name = scan.next();
+        //scan.nextLine();
+        System.out.print("\nEnter A Number for " + this.name + ": ");
+        this.phone = scan.next();
+        if(checkIfExisting(this.name,this.phone)){
+            System.out.println("\n--------------That contact already Exists---------------\n\n");
+        }
+        else {
+            currentContact[0] = this.name;
+            currentContact[1] = this.phone;
+            String temp = ("Name: " + this.name + "\t\t\tPhone: " + this.phone);
+            contacts.add(temp);
+            currentContact[0] = "";
+            currentContact[1] = "";
+        }
 
+    }
+
+    private boolean checkIfExisting(String name, String phone){
+        for (String w: contacts){
+            if(w.contains(name) && w.contains(phone)){
+                return true;
+            }
+        }
+        return false;
     }
 
     private void viewAllContact() {
@@ -64,6 +115,7 @@ public class Contacts {
         for (String w : contacts) {
             System.out.println(w);
         }
+        System.out.println("\n\n");
     }
 
     public void menu() {
